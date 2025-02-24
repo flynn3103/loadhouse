5a07fcf7b3db5e62852242502afb46263b362d0f
flynn <trannhatlinh3103@gmail.com>
2025-02-24 12:04:58 +0700

    add Spark Policy for [JAVA_GATEWAY_EXITED] Java gateway process exited before sending its port number.

--- tests/utils/exec_env_helpers.py
+++ tests/utils/exec_env_helpers.py
@@ -1,10 +1,9 @@
 """Module with helper functions to interact with test execution environment."""
-import sys
+
 import os
+import sys
 
-# Add the parent directory of lakehouse_engine to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-
+# Import after sys.path modification
 from lakehouse_engine.core.exec_env import ExecEnv
 
 class ExecEnvHelpers(object):
@@ -19,14 +18,18 @@ class ExecEnvHelpers(object):
             config={
                 "spark.master": "local[2]",
                 "spark.driver.memory": spark_driver_memory,
-                "spark.sql.warehouse.dir": "file:///app/tests/lakehouse/spark-warehouse/",  
+                "spark.sql.warehouse.dir": "file:///app/tests/lakehouse/spark-warehouse/",
                 "spark.sql.shuffle.partitions": "2",
                 "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
-                "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",  
-                "spark.jars.packages": "io.delta:delta-spark_2.12:3.2.0,org.xerial:sqlite-jdbc:3.45.3.0,com.databricks:spark-xml_2.12:0.18.0",  
+                "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+                "spark.jars.packages": "io.delta:delta-spark_2.12:3.2.0,org.xerial:sqlite-jdbc:3.45.3.0,com.databricks:spark-xml_2.12:0.18.0",
                 "spark.jars.excludes": "net.sourceforge.f2j:arpack_combined_all",
                 "spark.sql.sources.parallelPartitionDiscovery.parallelism": "2",
                 "spark.sql.legacy.charVarcharAsString": True,
+                "spark.driver.extraJavaOptions": "-Xss4M -Djava.security.manager=allow -Djava.security.policy=spark.policy",
+                "spark.authenticate": "false",
+                "spark.network.crypto.enabled": "false",
+                "spark.ui.enabled": "false",
             },
         )
 
